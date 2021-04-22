bca8f0ed8463beb789bfc597667e0e6cf129ab36
dliu328 <dliu328@gatech.edu>
2021-04-22 00:57:06 -0400

    change shell code

--- setup.sh
+++ setup.sh
@@ -1,12 +1,6 @@
-mkdir -p ~/.streamlit/
-echo "\
-[general]\n\
-email = \"diyi93@outlook.com\"\n\
-" > ~/.streamlit/credentials.toml
-echo "\
-[server]\n\
-headless = true\n\
-port = $PORT\n\
-enableCORS = false\n\
-\n\
+mkdir -p ~/.streamlit
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
