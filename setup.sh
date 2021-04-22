7decd70740f3a1a4284c54e72cedec4f982cb8c7
dliu328 <dliu328@gatech.edu>
2021-04-22 01:20:24 -0400

    set CORS false

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ echo "email = \"diyi93@outlook.com\""  >> ~/.streamlit/credentials.toml
 echo "[server]"  > ~/.streamlit/config.toml
 echo "headless = true"  >> ~/.streamlit/config.toml
 echo "port = $PORT"  >> ~/.streamlit/config.toml
-echo "enableCORS = true"  >> ~/.streamlit/config.toml
+echo "enableCORS = false"  >> ~/.streamlit/config.toml
