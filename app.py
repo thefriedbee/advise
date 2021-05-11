b7985096b9c0d11b6624d6f7d0ae59ef8734104a
dliu328 <dliu328@gatech.edu>
2021-05-11 04:21:08 -0400

    add bottom chart

--- app.py
+++ app.py
@@ -8,12 +8,13 @@ import seaborn as sn
 import streamlit as st
 import pydeck as pdk
 import shapely.wkt
+import altair as alt
 
 # SETTING PAGE CONFIG TO WIDE MODE
-st.set_page_config(layout="wide", page_title="Ex-stream-ly Cool App",
+st.set_page_config(layout="wide", page_title="ADVISE Interactive Dashboard",
                   page_icon="🧊", initial_sidebar_state="expanded",)
 
-@st.cache(persist=True, allow_output_mutation=True)
+# @st.cache(persist=True, allow_output_mutation=True)
 def load_gdf():
     def parse_geometry(x):
         return shapely.wkt.loads(x["geometry"])
@@ -29,35 +30,37 @@ def load_gdf():
     return gdf
 
 
-st.title("Exploring Advisè Corridor Reliability")
+st.title("Exploring ADVISE Corridor Reliability")
 
-row1_1, row1_2, row1_3 = st.beta_columns((2,9,1))
+row1_2, row1_3 = st.beta_columns((9,1))
+row2_2, row2_3 = st.beta_columns((9,1))
 
-with row1_1:
-    with st.beta_container():
-        region_radio = st.radio(
-            "Select region to Zoom in:",
-            ("Tennessee", "Knoxville", "Chattanooga", "Nashville", "Memphis"),
-            index=0
-        )
-        time_radio = st.radio(
-            "Select time period to display:",
-            ("January", "Feburary", "March", "Quarter 1"),
-            index=0
-        )
-        type_radio = st.radio(
-            "Select type of data to report:",
-            ("LOTTR", "TTTR", "Excessive Dalay"),
-            index=0
-        )
+
+with st.sidebar.beta_container():
+    region_radio = st.radio(
+        "Select region to Zoom in:",
+        ("Tennessee", "Knoxville", "Chattanooga", "Nashville", "Memphis"),
+        index=0
+    )
+    time_radio = st.radio(
+        "Select time period to display:",
+        ("January", "Feburary", "March", "Quarter 1"),
+        index=0
+    )
+    type_radio = st.radio(
+        "Select type of data to report:",
+        ("LOTTR", "TTTR", "Excessive Dalay"),
+        index=0
+    )
+
 
 def get_prefix_by_time(time_radio):
     if time_radio == "January":
-        return 'Jan.'
+        return 'Jan'
     elif time_radio == "Feburary":
-        return 'Feb.'
+        return 'Feb'
     elif time_radio == "March":
-        return 'Mar.'
+        return 'Mar'
     else:
         return 'Q1'
 
@@ -69,7 +72,7 @@ def get_suffix_by_type(type_radio):
     if type_radio == "Excessive Dalay":
         return "_ed"
 
-@st.cache(persist=True, allow_output_mutation=True)
+# @st.cache(persist=True, allow_output_mutation=True)
 def filt_df(df, region_radio):
     if region_radio == 'Tennessee':
         return df.copy()
@@ -86,7 +89,7 @@ def filt_df(df, region_radio):
     return df
 
 
-@st.cache(persist=True, allow_output_mutation=True)
+# @st.cache(persist=True, allow_output_mutation=True)
 def find_best_view(df, region_radio):
     mid_lats, mid_longs = df.MidLat, df.MidLong
     m_lat = statistics.median(mid_lats)
@@ -107,7 +110,7 @@ def plot_color_gradients(cmap, vmin, vmax):
     ax.remove()
     return fig
 
-@st.cache(persist=True, allow_output_mutation=True)
+# @st.cache(persist=True, allow_output_mutation=True)
 def get_map(df, lat, lon, zoom, region_radio, time_radio, type_radio):
     prefix_n = get_prefix_by_time(time_radio)
     suffix_n = get_suffix_by_type(type_radio)
@@ -173,9 +176,36 @@ with row1_3:
     st.pyplot(plot_color_gradients('plasma', vmin, vmax))
 
 
+def altris_compare_by_tot_miles_test(df, x_col, y_col, facet_col='city_name', use_log=True):
+    df = df[[x_col, y_col, facet_col]]
+    x_str = "{}:Q".format(x_col)
+    y_str = "sum({}):Q".format(y_col)
+    f_str = "{}:N".format(facet_col)
+    my_bin = alt.Bin(extent=[1.0, 3.5], step=0.02) # alt.Bin(maxbins=100)
+    x_axis = alt.X(x_str, bin=my_bin, scale=alt.Scale(type='log'))
+    if use_log is False:
+        x_axis = alt.X(x_str, bin=my_bin)
+    return alt.Chart(df).mark_bar(opacity=0.8,
+    ).encode(
+        x=x_axis,
+        y=alt.Y(y_str, stack=None, title="Total miles"),
+        color=alt.Color(f_str, sort=city_order, title="city names"),
+        tooltip=[alt.Tooltip(y_str, format='.2f')],
+        row=alt.Row(f_str, sort=city_order)
+    ).properties(height=50, width=800).interactive()
 
+with row2_2:
+    df1 = pd.DataFrame(gdf)
+    city_order = ['Knoxville', 'Chattanooga', 'Nashville', 'Memphis']
+    prefix_n = get_prefix_by_time(time_radio)
+    suffix_n = get_suffix_by_type(type_radio)
+    col_name = prefix_n + suffix_n
 
+    line = altris_compare_by_tot_miles_test(df1, col_name, 'Miles')
+    if suffix_n == "_ed":
+        line = altris_compare_by_tot_miles_test(df1, col_name, 'Miles', use_log=False)
 
+    st.altair_chart(line, use_container_width=True)
 
 
 
